2f0b303cb93d535fd3867b7f0ec906dd0eb76810
soniagurram <148077099+soniagurram@users.noreply.github.com>
2025-10-21 19:08:59 +0530

    Enhance employee model test with additional fields
    Expanded employee data in test to include department, email, salary, and is_active fields.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,26 @@
+# from app.models import EmployeeCreate, EmployeeOut
+
+
+# def test_employee_model():
+#     data = {"name": "Bob", "role": "Manager"}
+#     emp = EmployeeCreate(**data)
+#     out = EmployeeOut(id="123", **data)
+#     assert out.name == "Bob"
+
+
 from app.models import EmployeeCreate, EmployeeOut
 
 
 def test_employee_model():
-    data = {"name": "Bob", "role": "Manager"}
+    data = {
+        "name": "Bob",
+        "role": "Manager",
+        "department": "devops2",
+        "email": "Bob@gmail.com",
+        "salary": 50000,
+        "is_active": True
+    }
     emp = EmployeeCreate(**data)
     out = EmployeeOut(id="123", **data)
     assert out.name == "Bob"
+
